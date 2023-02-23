734941bcf8867dda014bc00b23b6e145c2d39012
Basil Pulikuth <73985514+iambasilp@users.noreply.github.com>
2023-02-23 16:32:58 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, {useReducer} from 'react'
+import './styles.css';
+const initialState = {
+  count:0,
+};
+console.log(initialState.count);
+
+const reducer = (state, action)=>{
+  switch(action.type){
+    case "Increament":
+      return {count:state.count + 1}
+    case "Decreament":
+      return {count:state.count - 1}
+    case "Reset":
+      return {count:action.payload}
+    default:
+      throw Error("error")
+  }
+}
+
+const App = ()=>{
+  const [state, dispatch] = useReducer(reducer, initialState)
+  return (
+    <div className="counter">
+      
+    <button className="button-3" onClick={()=>{dispatch({type:"Reset",payload:0})}}>Reset</button>
+      <button className="btn" onClick={()=>{dispatch({type:"Increament"})}}>Increament</button>
+      <h1>{state.count}</h1>
+      <button className="btn" onClick={()=>{dispatch({type:"Decreament"})}}>Decreament</button>
+    </div>
+  )
+}
+export default App
